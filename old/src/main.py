6754d269ed61f9873f6ed7ec8515975073f96a3f
Anton Forsman <10177340+anforsm@users.noreply.github.com>
2023-03-06 23:37:17 +0100

    moved a bunch of files

---
+++ old/src/main.py
@@ -0,0 +1,36 @@
+from tokenizer import Tokenizer
+from blobmanager import BlobManager
+
+
+tokenizer = Tokenizer()
+blobmanager = BlobManager()
+
+def test_decode():
+    tokenizer.audio_tokenizer.encodec_model.set_target_bandwidth(1.5)
+    tokens = tokenizer.tokenize("./../data/CV/cv-corpus-12.0-delta-2022-12-07/en/clips/common_voice_en_35095766.mp3", "test")
+    print(len(tokens))
+    tokenizer.detokenize(tokens, "./outputs/test_decode.wav", "./outputs/test_decode.txt")
+
+def create_blobs():
+    blobs = blobmanager.create_blobs()
+    blobmanager.save_blobs_to_file(blobs, "src/outputs/blobs.json")
+    #blobmanager.save_blobs_to_file(blobmanager.tokenize_blobs(tokenizer, blobs), "./outputs/blobs_tokenized.json")
+
+def tokenize_cv_blobs():
+    blobmanager.save_blobs_to_file(blobmanager.tokenize_blobs(tokenizer, blobmanager.load_blobs_from_file("src/outputs/blobs_rel_path.json")), "./outputs/blobs_tokenized.json")
+    #print(blobs)
+
+def downloader_CV_data():
+    from huggingface_hub import hf_hub_download
+    #hf_hub_download(repo_id="mozilla-foundation/common_voice_11_0", filename="fleurs.py", repo_type="dataset")
+    #blobmanager.save_blobs_to_file(blobs, "./outputs/blobs.json")
+    #blobmanager.save_blobs_to_file(blobmanager.tokenize_blobs(tokenizer, blobs), "./outputs/blobs_tokenized.json")
+    #print(blobs)
+
+def test_decode_blob():
+    tokens = blobmanager.load_blobs_from_file("./outputs/blobs_tokenized.json")[213]
+    tokenizer.detokenize(tokens, "./outputs/test_decode_blob.wav", "./outputs/test_decode_blob.txt")
+
+if __name__ == "__main__":
+    #create_blobs()
+    tokenize_cv_blobs()
